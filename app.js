eb791efafda6920e0dd3be40b9e6b135388b8045
patocamus <patricio.ismael.camus@gmail.com>
2025-02-18 01:09:25 -0300

    0.1:  Recibe el nombre digitado, niega nombres con números o en blanco, imprime en pantalla lo digitado.

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = []; //Declaración del array que contendrá la lista de amigos.
+
+function agregarAmigo(){ //Declaración de la función que permite agregar amigos a la lista.
+    let datoUsuario = document.getElementById('amigo').value.trim();
+    if(datoUsuario === '') {
+        alert("Por favor, inserte un nombre.");
+        return;
+    } if (amigos.includes(datoUsuario)) {
+        alert("¡Ya añadiste a ese amigo a la lista!")
+        return;
+    } if (/\d/.test(datoUsuario)) {
+        alert("¡El nombre de tu amigo no puede contener números!");
+    }
+    else {
+        amigos.push(datoUsuario);
+        imprimirAmigo();
+        limpiarNombre();
+        return;
+    }
+}
+function imprimirAmigo (){
+    let lista = document.getElementById('listaAmigos');
+    let nuevoDatoLista = document.createElement('li');
+    nuevoDatoLista.textContent = amigos[amigos.length - 1];
+    lista.appendChild(nuevoDatoLista);
+    return;
+}
+function limpiarNombre (){
+    document.getElementById('amigo').value = '';
+    return;
+}
